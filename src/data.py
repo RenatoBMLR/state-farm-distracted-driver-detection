a22c030c31a6cbee776b9dfcd9e0f8e0adc62fd8
RenatoBMLR <renatobmlr@gmail.com>
2017-10-16 20:31:36 -0200

    class to get the data

---
+++ src/data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 20:06:00 2017
+
+@author: renatobottermaiolopesrodrigues
+"""
+
+
+from PIL import Image
+import os
+import nuumpy as np
+import pandas as pd
+import glob
+
+class get_data():
+    def __init__(self, path2data, path2labels, img_rows =224, img_cols=224 ):
+        self.path2data = path2data
+        self.path2labels = path2labels
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+
+    def load_data(self):
+        
+        self.trainLabels = pd.read_csv(self.path2labels)
+
+        immatrix = []
+        imlabel = []
+        
+        files = glob.glob (self.path2data + "*.PNG")
+        for myFile in files:    
+            base = os.path.basename(myFile)
+            fileName = os.path.splitext(base)[0]
+            imlabel.append(self.trainLabels.loc[self.trainLabels.image==fileName, 'level'].values[0])
+            im = Image.open(myFile)
+            img = im.resize((self.img_rows,self.img_cols))
+            immatrix.append(np.array(img))    
+             
+        self.immatrix = np.asarray(immatrix)
+        self.imlabel = np.asarray(imlabel)
+
+
+
+
+        
+    
+    
+    
+        
+        
