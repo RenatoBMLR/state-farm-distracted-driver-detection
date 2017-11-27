dfa0a977791b4fee0af25aca51f75365d0818f98
EulerFaria <eulerodriguesousa@gmail.com>
2017-11-27 00:29:12 +0000

    Função torch_summarize

--- src/extractor_utils.py
+++ src/extractor_utils.py
@@ -3,6 +3,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 import numpy as np
+from torch.nn.modules.module import _addindent
 
 #********************************************           Predicting Features       *********************************************** 
 
@@ -27,7 +28,7 @@ def predict(dset_loaders, model,use_gpu=False):
             inputs = inputs.cuda()
             labels = labels.cuda()
             
-        inputs = Variable(inputs,volatile=True)
+        inputs = Variable(inputs)
         predictions.append(model(inputs).data)
         labels_lst.append(labels)
 
@@ -92,3 +93,31 @@ def features_loading(path2data,model_name,use_gpu=False):
             print("pred {}, true {}".format(data[i][0].shape,data[i][1].shape))
 
     return data
+
+def torch_summarize(model, show_weights=True, show_parameters=True):
+    """Summarizes torch model by showing trainable parameters and weights."""
+    tmpstr = model.__class__.__name__ + ' (\n'
+    for key, module in model._modules.items():
+        # if it contains layers let call it recursively to get params and weights
+        if type(module) in [
+            torch.nn.modules.container.Container,
+            torch.nn.modules.container.Sequential
+        ]:
+            modstr = torch_summarize(module)
+        else:
+            modstr = module.__repr__()
+        modstr = _addindent(modstr, 2)
+
+        params = sum([np.prod(p.size()) for p in module.parameters()])
+        weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+        tmpstr += '  (' + key + '): ' + modstr 
+        if show_weights:
+            tmpstr += ', weights={}'.format(weights)
+        if show_parameters:
+            tmpstr +=  ', parameters={}'.format(params)
+        tmpstr += '\n'   
+
+    tmpstr = tmpstr + ')'
+    return tmpstr
+
